57c605cda5715659a5e64dbf023235e05e6e4328
usamplazo <petrovicgligorijeTSI10B@gmail.com>
2023-12-10 16:26:21 +0100

    Setting up project(editorConfig, Docker compose, Serilog)

--- OfficeLeagueAPI/Program.cs
+++ OfficeLeagueAPI/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
 
 var app = builder.Build();
 
